cd397af4a759c75dc54fb910621d32ec77935530
Sourabh <sourabh4pvt@gmail.com>
2024-04-15 16:26:53 +0530

    created contest provider

--- src/store/DisplaySettingStore.jsx
+++ src/store/DisplaySettingStore.jsx
@@ -1,5 +1,21 @@
 import { createContext } from "react";
 
 export const DisplaySetting = createContext({
-    
+  initial: [],
 });
+
+const DisplaySettingContextProvider = ({ children }) => {
+
+
+  return (
+    <DisplaySetting.Provider
+      value={{
+        initial
+      }}
+    >
+      {children}
+    </DisplaySetting.Provider>
+  );
+};
+
+export default DisplaySettingContextProvider;
