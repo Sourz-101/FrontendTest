aa1e6633e1ec9b5d2f09a54faf47e8421d40a158
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-04-18 16:31:11 +0530

    option select

--- src/components/MachineSelector.jsx
+++ src/components/MachineSelector.jsx
@@ -16,7 +16,7 @@ const MachineSelector = ({ array, onSelect }) => {
         onChange={handleChange}
         className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
       >
-        <option></option>
+        <option>Select</option>
          {array.map((option, index) => (
           <option key={index} value={option}>
             {option}
