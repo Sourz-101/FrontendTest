437ee623720410160e7df149794ba44b3cccf811
Sourabh <sourabh4pvt@gmail.com>
2024-04-18 13:59:53 +0530

    logic logic

--- src/components/PropSelector.jsx
+++ src/components/PropSelector.jsx
@@ -17,6 +17,7 @@ const PropSelector = ( { array, onSelect}) => {
         onChange={handleChange}
         className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
       >
+      <option>Select a value</option>
          {array.map((option, index) => (
           <option key={index} value={option}>
             {option}
